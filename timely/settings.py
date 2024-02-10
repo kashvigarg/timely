38d78f2f92a72008457553c7efc426d9c0d80f6f
kashvigarg <kashvigarg02@gmail.com>
2024-02-10 16:14:21 +0530

    modified CORS settings

--- timely/settings.py
+++ timely/settings.py
@@ -31,7 +31,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework_swagger',
-    'rest_framework',               
+    'rest_framework',     
+    'corsheaders',          
     'rest_framework.authtoken',
     'timelyapp.apps.TimelyappConfig'
 ]
@@ -47,10 +48,14 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "timely.urls"
 AUTH_USER_MODEL = 'timelyapp.CustomUser'
